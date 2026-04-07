d6bfb1a055062612a26979dadb668fbc9117ff39
Brandon Quenallata <bquenallata@frba.utn.edu.ar>
2026-04-07 19:59:07 -0300

    archivos tp

---
+++ 00-CHelloworld/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Anotaciones personales
+// Para ejecutar código se utiliza gcc "archivo.c" -o nombreprograma donde se genera un ejecutable y en el cmd se hace .\nombreprograma.exe
+// Para redireccionarlo a un .txt: .\nombreprograma.exe > nombresalida.txt
+
+int main(void)
+{
+printf("Hello world\n");
+printf("La version del GCC utilizado es\n 14.2.0\n");
+return 0;
+}
